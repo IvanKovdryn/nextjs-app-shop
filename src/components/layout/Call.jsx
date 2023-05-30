584b5a8509b010d57317b7bf5c2a84fca553cf21
Ivan Kovdryn <ivankovdrin7@gmail.com>
2023-05-30 15:01:31 +0300

    correct sending form

--- src/components/layout/Call.jsx
+++ src/components/layout/Call.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "./header/nav/Nav.module.css";
+import { ShopService } from "@/services/shop.service";
 
 const Call = () => {
   const [call, setCall] = useState(false);
